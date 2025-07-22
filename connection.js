ce11214f9e3f34ab0188b09916f27cca4a8e5040
montrell43 <tmerriweather@persevernow.org>
2025-07-22 12:14:19 -0500

    add commit

--- connection.js
+++ connection.js
@@ -3,6 +3,7 @@ const client = new MongoClient(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+console.log('Mongo URI:', process.env.MONGO_URI);
 
 module.exports = async function (callback) {
   try {
