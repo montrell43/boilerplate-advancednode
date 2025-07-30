201716e9b0e23b830d35e1d607cd9cdbda8cc6d3
montrell43 <tmerriweather@persevernow.org>
2025-07-30 12:01:36 -0500

    new commit

--- auth.js
+++ auth.js
@@ -1,27 +1,40 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const { ObjectId } = require('mongodb');
 
-module.exports = function (app, myDataBase) {
-  // Local Strategy
-  passport.use(new LocalStrategy((username, password, done) => {
-    myDataBase.findOne({ username: username }, (err, user) => {
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (user.password !== password) return done(null, false);
-      return done(null, user);
-    });
-  }));
+module.exports = function(app, myDataBase) {
+  passport.use(new LocalStrategy(
+    (username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    }
+  ));
 
-  // Serialize
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
 
-  // Deserialize
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectId(id) }, (err, doc) => {
-      done(null, doc);
+      done(err, doc);
+    });
+  });
+
+  // Registration route (optional: you can put it here or in server.js)
+  app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    myDataBase.findOne({ username: username }, (err, user) => {
+      if (err) return res.redirect('/');
+      if (user) return res.redirect('/'); // User exists, redirect back
+
+      myDataBase.insertOne({ username, password }, (err, doc) => {
+        if (err) return res.redirect('/');
+        res.redirect('/');
+      });
     });
   });
 };
--- server.js
+++ server.js
@@ -3,16 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
-const path = require('path');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const myDB = require('./connection');
-const routes = require('./routes');
 const auth = require('./auth');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views/pug'));
+app.set('views', path.join(__dirname, 'views', 'pug'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,29 +20,30 @@ fccTesting(app);
 app.use(session({
   secret: process.env.SESSION_SECRET || 'keyboard cat',
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 myDB(async (client) => {
-  const myDataBase = await client.db('exercise-tracker').collection('users');
+  const myDataBase = client.db('exercise-tracker').collection('users');
 
   auth(app, myDataBase);
-  routes(app, myDataBase);
 
+  // This POST /login route **must** be here
   app.post('/login',
     passport.authenticate('local', { failureRedirect: '/' }),
     (req, res) => {
-      res.redirect('/');
-    });
+      res.redirect('/');  // Redirect to '/' on successful login
+    }
+  );
 
   app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port ' + (process.env.PORT || 3000));
   });
-}).catch((err) => {
+}).catch((e) => {
   app.route('/').get((req, res) => {
-    res.render('index', { title: err, message: 'Unable to connect to database' });
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
